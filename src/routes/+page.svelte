f0a658936d2b369507346ebeb31aedf7ff7e8d8a
Collin Ferguson <collinleeferguson@gmail.com>
2025-02-25 21:44:57 -0500

    adds basic header bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,22 @@
 <script>
-    import{Button, Card, Page, PageBody, SelectorInput} from 'ascend-ui'
+    import{Button, Card, LoginCard, LoginPage, Page, PageBody, SelectorInput} from 'ascend-ui'
     export let data;
     const optionsList = ["Super Admin User","Supervisor User","Home User"]
 
 </script>
-
-<Page>
-	<PageBody size="full" styles={["display:flex", "justify-content: center", " align-items: center", "height: 100vh"]}>
-        <h1>Auth Page!</h1>
-        <Card classes={["card-l"]} styles={["min-height: 60vh", "min-width: 40vw"]}>
-        <div style="width:100%"> 
-            <SelectorInput
-            bind:selectedValue={data.selectedUser}
-            defaultOptionValue="0"
-            id = "login-selector"
-            label="Select Login Type"
-            optionList = {optionsList}
-            /><br>
-            <Button styles={[]} text = "Login" callback={data.submitLogin}></Button>
-
-        </div>
-    </Card>
-    </PageBody>
+<Page styles={["max-height:95vh"]}>
+<div style="height:80vh !important;">
+    <LoginPage>
+        <!--<PageBody size="full" styles={["display:flex", "justify-content: center", " align-items: center", "height: 100vh"]}>-->
+                <SelectorInput
+                bind:selectedValue={data.selectedUser}
+                defaultOptionValue="0"
+                id = "login-selector"
+                label="Select Login Type"
+                optionList = {optionsList}
+                /><br>
+                <Button styles={[]} text = "Login" callback={data.submitLogin}></Button>
+        <!--</PageBody>-->
+    </LoginPage>
+</div>
 </Page>
--- src/routes/containers/staff/+page.svelte
+++ src/routes/containers/staff/+page.svelte
@@ -1,16 +1,48 @@
 <script>
-	import { Accordion, MainContent, Button, PageBody, Page, Card, NewNotificationIcon } from 'ascend-ui'
+	import { Accordion, MainContent, Button, PageBody, Page, Card, NewNotificationIcon, PageHeader, MetricCard } from 'ascend-ui'
     import {GitHubIcon, LinkedInIcon} from '$lib/index'
 
     export let data;
-
 </script>
 
-<Page>
-	<PageBody size="full">
+<Page styles={["horizontal-padding:5vw", "justify-content:left"]}>
+    <div style="display:column; align-items:top; justify-content:left; width:95%">
+        <div style=" width:100%; height:8vw;">
+            <PageHeader titleText="Staff" titleJustification="left" styles={["border-left: 4px solid #E0E7FF;"]} >
+            </PageHeader>
+        </div>
+        <div style="display:flex; justify-content:left; width:100%; justify-content:space-between; padding-left:5vw; padding-top:45px; min-width:100">
+            <!--The Metric Card components do not have classes or else I would just give them a custom class-->
+            <MetricCard
+            styles={['min-width:120px', 'align-items:center', "max-height:100px", "box-shadow:4px 4px 10px rgba(0, 0, 0, 0.3)"]}
+            title="My Staff"
+            value="10"
+            />
+            <MetricCard
+            styles={['min-width:120px', 'align-items:center', "max-height:100px", "box-shadow:4px 4px 10px rgba(0, 0, 0, 0.3)"]}
+            title="Total Staff"
+            value="100"
+            />
+            <MetricCard
+            styles={['min-width:120px', 'align-items:center', "max-height:100px", "box-shadow:4px 4px 10px rgba(0, 0, 0, 0.3)"]}
+            title="Archived Staff"
+            value="10"
+            />
+        </div>
+
+    </div>
+    <PageBody size="full">
         <div class="borders"> 
             <h1>Welcome {data.user.firstName} {data.user.lastName}!</h1>
         </div>
     </PageBody>
 </Page>
 
+<style>
+.metric-card {
+
+
+}
+
+
+</style>
